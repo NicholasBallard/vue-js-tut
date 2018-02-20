9d2f7539cbb8033055cec428bf731978f4f6c544
NicholasBallard <nicholasericballard@gmail.com>
2018-02-20 09:43:53 -0600

    did IF conditionals and V-SHOW

--- app.js
+++ app.js
@@ -1,16 +1,9 @@
 new Vue({
     el: "#vue-app",
     data: {
-        available: false,
-        nearby: false,
+        error: true,
+        success: true,
     },
     methods: {},
-    computed: {
-        compClasses: function () {
-            return {
-                available: this.available,
-                nearby: this.nearby,
-            }
-        },
-    },
+    computed: {},
 });
